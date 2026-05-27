176c7607660f9682b3769b008094b85bf61d6f62
Andrei Pustovoit <andreipustovoit6@gmail.com>
2026-05-27 21:51:48 +0300

    completed wizard page task

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,12 @@
-import { HomeOutlined, TableOutlined } from "@ant-design/icons";
+import { FormOutlined, HomeOutlined, TableOutlined } from "@ant-design/icons";
 import Home from "./pages/Home.jsx";
 import Coins from "./pages/Coins.jsx";
 import CoinsPaged from "./pages/CoinsPaged.jsx";
+import Wizard from "./pages/Wizard.jsx";
 
 export const routes = [
     { path: "/", label: "Home", icon: <HomeOutlined />, element: <Home /> },
     { path: "/coins", label: "Coins", icon: <TableOutlined />, element: <Coins /> },
-    {path: "/coins-paged", label: "Coins-paged", icon: <TableOutlined />, element: <CoinsPaged />}
+    {path: "/coins-paged", label: "Coins-paged", icon: <TableOutlined />, element: <CoinsPaged />},
+    {path: "/wizard", label: "Form", icon: <FormOutlined />, element: <Wizard/>}
 ];
